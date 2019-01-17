bf2d1e0a0475e8be966607d54d66576204cfd5bf
Mins <heroms01@gmail.com>
2019-01-17 13:21:37 +0900

    3.3 접시무더기 리펙토링

--- Chapter3/SetOfStacks.java
+++ Chapter3/SetOfStacks.java
@@ -1,18 +1,16 @@
+import org.apache.poi.ss.formula.functions.T;
+
 import java.util.ArrayList;
 
 class SetOfStacks {
-    //스택리스트
     ArrayList<ArrayList<Integer>> stackList = new ArrayList();
-    
-    //스택 용량
     int capacity = 2;
 
     public void push(Integer value) {
-        // 스택리스트가 비었거나 스택 용량 가득 찼다면 새로운 스택 생성
-        if (stackList.isEmpty() || stackList.get(stackList.size()-1).size() >= capacity)
+        if (stackList.isEmpty() || ((ArrayList<Integer>)getLastArrayListObject(stackList)).size() >= capacity)
             stackList.add(new ArrayList());
 
-        stackList.get(stackList.size() - 1).add(value);
+        ((ArrayList<Integer>)getLastArrayListObject(stackList)).add(value);
     }
 
     public Integer pop() {
@@ -21,19 +19,25 @@ class SetOfStacks {
         }
 
         //가장 최근 스택 가져오기
-        ArrayList<Integer> stack = stackList.get(stackList.size() - 1);
+        ArrayList<Integer> stack = (ArrayList<Integer>)getLastArrayListObject(stackList);
+
 
         //마지막 값 가져오기
-        Integer value = stack.get(stack.size() - 1);
+        Integer value = (Integer)getLastArrayListObject(stack);
 
         //마지막 데이터 삭제
         stack.remove(stack.size() - 1);
 
-        //현재 스택 비웠으면 스택리스트 에서 삭제
+        //현재 스택 비웠으면 스택 컬렉션에서 삭제
         if (stack.isEmpty())
             stackList.remove(stackList.size()-1);
 
         return value;
+    }
+
+    //리스트의 마지막 데이터 가져오기
+    private Object getLastArrayListObject(ArrayList arrayList) {
+        return arrayList.get(arrayList.size() - 1);
     }
 
     public static void main(String[] args) {
